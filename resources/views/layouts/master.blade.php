8c978acbe652430fb6a0aead086ab964d68eadf6
FAIZAN HAFEEZ <mfaizan.hafeez@arhamsoft.org>
2024-04-24 21:19:07 +0500

    completed code and implemented the yajra

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,21 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
+    <script src="https://cdn.tailwindcss.com"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     @yield('css')
 </head>
 
 <body>
-
     @yield('content')
 
 
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
-
+@include('layouts.navigation')
 
 @section('css')
     <style>
@@ -9,29 +9,20 @@
 
 
 @section('content')
-    <div class="container">
-        <h1 class="text-center text-success mt-5 mb-5"><b>Laravel 10 Datatables Date Range Filter</b></h1>
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col col-9"><b>Sample Data</b></div>
-                </div>
-            </div>
-            <div class="card-body">
-                <table class="table table-bordered" id="daterange_table">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Created On</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <div class="container mx-auto px-4 lg:px-8 py-8">
+        <h2 class="text-center text-2xl font-bold mb-4">Data Tables Using Ajax & Yajra</h2>
+        <table id="daterange_table" class="table-auto w-full">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2">No</th>
+                    <th class="px-4 py-2">Name</th>
+                    <th class="px-4 py-2">Email</th>
+                    <th class="px-4 py-2">Created On</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
     </div>
 @endsection
 
@@ -61,7 +52,10 @@
                         data: 'created_at',
                         name: 'created_at'
                     }
-                ]
+                ],
+                "createdRow": function(row, data, dataIndex) {
+                    $('td', row).addClass('border px-4 py-2');
+                }
             });
         });
     </script>
